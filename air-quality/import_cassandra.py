2cf456a2db9a71d44748f05cff384c6269306622
ludovic <ludovic.sinquin@gmail.com>
2017-02-27 03:13:24 +0100

    Mise en place des paramètres de lancement

--- air-quality/import_cassandra.py
+++ air-quality/import_cassandra.py
@@ -1,16 +1,19 @@
+#!/usr/bin/python
 
-
+import sys
+import csv
+import os
+import dateparser
 from datetime import datetime
 from cassandra.cluster import Cluster
-import csv
-import dateparser
+
 
 stations = [{'stationName' : 'auber', 'filePath' : 'data/qualite-de-lair-mesuree-dans-la-station-auber.csv'},
          {'stationName' : 'chatelet', 'filePath' : 'data/qualite-de-lair-mesuree-dans-la-station-chatelet.csv'},
          {'stationName' : 'franklin-d-roosevelt', 'filePath' : 'data/qualite-de-lair-mesuree-dans-la-station-franklin-d-roosevelt.csv'}]
 
 def importStationData(station): #read data from a csv station file and export to a cassandra cluster.
-    with open(station['filePath'], 'rb') as csvfile:
+    with open(os.path.join(os.path.dirname(__file__), station['filePath']), 'rb') as csvfile:
         reader = csv.DictReader(csvfile, delimiter=';')
 
         for row in reader:
@@ -19,9 +22,9 @@ def importStationData(station): #read data from a csv station file and export to
                 session.execute("""
                 INSERT INTO measures (time, station, no, no2, pm10, co2, temp, humi)
                 VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
-                """, (datetime.isoformat(), station['stationName'], convertToInt(row['NO']), convertToInt(row['NO2']), convertToInt(row['PM10']), convertToInt(row['CO2']), convertToInt(row['TEMP']), convertToInt(row['HUMI'])))
+                """, (datetime.isoformat(), station['stationName'], convertToInt(row['NO']), convertToInt(row['NO2']), convertToInt(row['PM10']), convertToInt(row['CO2']), convertToFloat(row['TEMP']), convertToFloat(row['HUMI'])))
             except:
-                pass
+                print("Impossible to import : ", row)
 
 def convertToInt(value):
     try:
@@ -29,11 +32,19 @@ def convertToInt(value):
     except ValueError:
         return None
 
-#By default the cluster is only composed of localhost node.
-cluster = Cluster()
-#Connect to the cluster and use the keyspace air_data
-session = cluster.connect('air_data')
+def convertToFloat(value):
+    try:
+        return float(value)
+    except ValueError:
+        return None
 
-# Import station data to Cassandra
-for station in stations:
-    importStationData(station)
+if(len(sys.argv) < 3):
+	print('A Cassandra host and Cassandra\'s port need to be provided as launch parameters : python your_path/import_cassandra.py host port')
+else:
+	cluster = Cluster([sys.argv[1]], port=sys.argv[2])
+	#Connect to the cluster and use the keyspace air_data
+	session = cluster.connect('air_data')
+
+	# Import station data to Cassandra
+	for station in stations:
+		importStationData(station)
--- air-quality/import_from_cassandra_to_elastic.py
+++ air-quality/import_from_cassandra_to_elastic.py
@@ -1,22 +1,28 @@
+#!/usr/bin/python
+
+import sys
+import dateparser
 from cassandra.cluster import Cluster
 from elasticsearch import Elasticsearch
-import dateparser
 
 
-cluster = Cluster()
-session = cluster.connect('air_data')
-es = Elasticsearch()
+if(len(sys.argv) < 5):
+	print('A Cassandra host, Cassandra\'s port, Elasticsearch\s host and ElasticSearch\'s port need to be provided as launch parameters : python your_path/import_cassandra.py cassandra_host cassandra_port elasticsearch_host elasticsearch_port')
+else:
+	cluster = Cluster([sys.argv[1]], port=sys.argv[2])
+	session = cluster.connect('air_data')
+	es = Elasticsearch(hosts=[{'host':sys.argv[3], 'port':sys.argv[4]}])
 
-rows = session.execute('SELECT * FROM measures')
-for row in rows:
-    measure = {
-    'timestamp': row.time,
-    'station': row.station,
-    'no': row.no,
-    'no2': row.no2,
-    'tempature': row.temp,
-    'humidity': row.humi,
-    'co2': row.co2,
-    'pm10': row.pm10
-    }
-    res = es.index(index='air-' + row.station, doc_type='measure', body=measure)
+	rows = session.execute('SELECT * FROM measures')
+	for row in rows:
+		measure = {
+		'timestamp': row.time,
+		'station': row.station,
+		'no': row.no,
+		'no2': row.no2,
+		'tempature': row.temp,
+		'humidity': row.humi,
+		'co2': row.co2,
+		'pm10': row.pm10
+		}
+		res = es.index(index='air-' + row.station, doc_type='measure', body=measure)
